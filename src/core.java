130c67478d9fe077edab234167ff4d40dafaa356
Rafael <78911513+R-Gasanov@users.noreply.github.com>
2024-06-29 13:19:23 +0100

    2nd Commit
    Made a little bit of progress, initiated the initial text and calling of a separate class now. Next step is to create an error handler for the scanner and

--- src/core.java
+++ src/core.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 
 public class core 
 {
-	public static void main(String args[])
+	public void StartGame()
 	{
-		System.out.println("Here is the test out whether or not this works!");
+		System.out.println("-------------Version 0.0.0.1--------------");
+		System.out.println("------------------------------------------");
+		System.out.println("------Enigma Games presents... KABIN------");
+		System.out.println("------------------------------------------");
+		System.out.println("\n");
+		
+		// Below is the initial dialogue that starts up.
+		
+		System.out.println("Your head felt heavy.\n"
+				+ "As you begin to come back to your senses, you notice where you are.\n"
+				+ "Your the middle of some woods."
+				+ "You get up from the dried crisp leaves that layed below you.\n"
+				+ "Wondering how you got here in the first place.\n"
+				+ "You tried to think but you're met with the stabbing pain in your head.\n"
+				+ "Only to realise besides the pain your mind is blank.\n"
+				+ "With no memories as to why you're here.\n"
+				+ "Or who you are.");
+		
+		
 	}
 
 }
---
+++ src/startup.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+
+public class startup 
+{
+	public static void main(String args[])
+	{
+		// Below we are displaying the version and a simple display of the game
+		System.out.println("-------------Version 0.0.0.1--------------");
+		System.out.println("------------------------------------------");
+		System.out.println("------Enigma Games presents... KABIN------");
+		System.out.println("------------------------------------------");
+		System.out.println("\n");
+		System.out.println("-Type 'ENTER' to start or 'EXIT' to leave-");
+		// Above is presenting the potential options on what to do
+		int count = 0;
+		
+		// While loop to make sure the error handler is fully checked and successful
+		while (count == 0)
+		{
+			// Using a scanner for an input
+			Scanner input = new Scanner(System.in);
+			String outcome;
+			// Using try and catch for error handling
+			try
+			{
+				outcome = input.nextLine();
+				System.out.println(outcome);
+				if (outcome.toLowerCase().equals("enter"))
+				{
+					count = 1;
+					
+					core gameinstance = new core();
+					gameinstance.StartGame();
+					
+				}
+				else if (outcome.toLowerCase().equals("exit"))
+				{
+					count = 2;
+				}
+				else
+				{
+					System.out.println("You've provided an incorrect string");
+					System.out.println("Please type specifically either...");
+					System.out.println("'Enter' OR 'Exit' for their following actions");
+				}
+			}
+			catch (Exception E)
+			{
+				System.out.println("Please enter a valid response in letters...");
+				System.out.println("'Enter' OR 'Exit' for their following actions");
+			}
+			//System.out.println(count);
+			//Above is what I've used to double check the end results of the user's input
+		}
+	}
+
+}
