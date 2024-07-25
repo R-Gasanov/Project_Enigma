7c5088e8e1043431ff4d67e6cfa3e79ef35bb715
Rafael <78911513+R-Gasanov@users.noreply.github.com>
2024-07-25 22:06:36 +0100

    1.4 Choice selection Kabin
    I've managed to design the relative puzzle that will occur within Figma. With the only additional thing being to implement the puzzle itself and the additional inventory items that will be used.

--- src/cabin.java
+++ src/cabin.java
@@ -65,6 +65,42 @@ public class cabin
 				checker = LocErrHandler(input);
 			}
 			checker = false;
+			
+			Spaceadder(2);
+			System.out.println("You enter inside the Kabin. \n"
+					+ "The moment you do, your headache worsens. \n"
+					+ "You take haste and attempt to rebalance yourself. \n"
+					+ "Finally after it weakens again you get to look around. \n"
+					+ "It looked suprisingly cosy,"
+					+ "with post it notes stuck around everywhere and furniture that made it look like a bedroom. \n"
+					+ "There was another room to your left as well too. Leading to the bathroom. \n"
+					+ "But wait... you haven't been here at all to know that? \n"
+					+ "But something in your mind tells you that this place is all to familiar. \n");
+			
+			System.out.println("Enter in Continue \n");
+			
+			while (checker == false)
+			{
+				checker = LocErrHandler(input);			
+			}
+			checker = false;
+			
+			// Here we have the choices for the story line the character can go through.
+			
+			while (checker == false)
+			{
+				int location = 0;
+				Spaceadder(2);
+				System.out.println("What do you choose to look at? \n"
+						+ "Enter '1' to look at the desk. \n"
+						+ "Enter '2' to look at the notice board. \n"
+						+ "Enter '3' to look at the cabinet. \n"
+						+ "Enter '4' to look at the bed. \n"
+						+ "Enter '5' to look at the bedside table. \n"
+						+ "Enter '6' to look at the bathroom door. \n"
+						+ "Enter '7' to go back. \n");
+				location = LocKabinHandler(input);
+			}
 		}
 		
 	}
@@ -103,5 +139,55 @@ public class cabin
 		}
 		return false;
 	}
-
+	
+	public static int LocKabinHandler(Scanner input)
+	{
+		String outcome;
+		try
+		{
+			outcome = input.nextLine();
+			
+			if (outcome.equals("1"))
+			{
+				return 1;
+			}
+			else if (outcome.equals("2"))
+			{
+				return 2;
+			}
+			else if (outcome.equals("3"))
+			{
+				return 3;
+			}
+			else if (outcome.equals("4"))
+			{
+				return 4;
+			}
+			else if (outcome.equals("5"))
+			{
+				return 5;
+			}
+			else if (outcome.equals("6"))
+			{
+				return 6;
+			}
+			else if (outcome.equals("7"))
+			{
+				return 7;
+			}
+		}
+		catch (Exception E)
+		{
+			System.out.println("Please enter a valid response in letters...");
+			System.out.println("'Enter' OR 'Exit' for their following actions");
+		}
+		
+		
+		return 0;
+	}
+	public static void desk()
+	{
+		Spaceadder(2);
+		
+	}
 }
